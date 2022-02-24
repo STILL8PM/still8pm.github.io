d17771dc37adb59b89b7f1052483bb4fcc1d4894
pw <1040479402@qq.com>
2022-02-24 23:05:22 +0800

    1

---
+++ myapp/src/component/Search/index.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import PubSub from 'pubsub-js'
+import axios from 'axios'
+export default class Search extends Component {
+
+	search = () => {
+		const { keyWordElement: { value: keyWord } } = this
+		//发送前请求前通知List更新状态
+		PubSub.publish('atguigu',{ isFirst: false, isLoading: true })
+		//发送网络请求
+		axios.get(`/api1/search/users?q=${keyWord}`).then(
+			response => {
+				//请求成功后通知List更新状态
+				PubSub.publish('atguigu',{ isLoading: false, users: response.data.items })
+			},
+			error => {
+				//请求失败后通知List更新状态
+				PubSub.publish('atguigu',{ isLoading: false, err: error.message })
+			}
+		)
+	}
+
+	handleKeyUp = (event) => {
+		const { keyCode } = event
+		if (keyCode !== 13) return
+		const { keyWordElement: { value: keyWord } } = this
+		//发送前请求前通知List更新状态
+		PubSub.publish('atguigu',{ isFirst: false, isLoading: true })
+		//发送网络请求
+		axios.get(`/api1/search/users?q=${keyWord}`).then(
+			response => {
+				//请求成功后通知List更新状态
+				PubSub.publish('atguigu',{ isLoading: false, users: response.data.items })
+			},
+			error => {
+				//请求失败后通知List更新状态
+				PubSub.publish('atguigu',{ isLoading: false, err: error.message })
+			}
+		)
+	}
+
+	render() {
+		return (
+			<section className="jumbotron">
+				<h3 className="jumbotron-heading">搜索Github用户</h3>
+				<div>
+					<input onKeyUp={this.handleKeyUp} ref={c => this.keyWordElement = c} type="text" placeholder="输入关键词点击搜索" />&nbsp;
+					<button onClick={this.search}>搜索</button>
+				</div>
+			</section>
+		)
+	}
+}
